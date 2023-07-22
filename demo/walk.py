81de80b6f9b7b0239674fd7d5a767bba31caee05
tyama01 <tyama@inl.ics.keio.ac.jp>
2023-07-22 15:12:52 +0900

    commit

--- demo/walk.py
+++ demo/walk.py
@@ -11,8 +11,38 @@ def random_walk(G, v, walk_num, v_walk_num_cnt):
         else:
             v_walk_num_cnt[v] = 1
             
-        neigbors = list(G.neighbors(v))
-        random_index = random.randrange(len(neigbors))
-        v = neigbors[random_index]
+        neighbors = list(G.neighbors(v))
+        random_index = random.randrange(len(neighbors))
+        v = neighbors[random_index]
     
     return v_walk_num_cnt
+
+
+# 近似 PageRank
+
+def pr_random_walk(G, v, walk_num, v_walk_num_cnt, d):
+    
+    node_list = list(G.nodes)
+    
+    for i in range(walk_num + 1):
+        
+        if(v in v_walk_num_cnt):
+            v_walk_num_cnt[v] += 1
+        else:
+            v_walk_num_cnt[v] = 1
+            
+        r_value = random.random()
+        
+        if(r_value <= d):
+            neighbors = list(G.neighbors(v))
+            random_index = random.randrange(len(neighbors))
+            v = neighbors[random_index]
+        else:
+            random_index = random.randrange(len(node_list))
+            v = node_list[random_index]
+    
+    return v_walk_num_cnt
+    
+            
+            
+        
